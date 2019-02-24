52a8d4ef11729fbffc53684801b6b039fe33f680
sapatelbaps <sapatelbaps@gmail.com>
2019-02-23 20:41:26 -0800

    How to declare functions in TS and create and use classes also.

---
+++ main1.js
@@ -0,0 +1,5 @@
+var log = function (message) {
+    console.log(message);
+};
+var doLog = function (message) { return console.log(message); };
+var doLog1 = function () { return console.log(' Without parameter message'); };
